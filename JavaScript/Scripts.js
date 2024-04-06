294d2609a6a2faaf870f7fd81bd60166822e293e
EfrenDaMo <david.mora.work@gmail.com>
2024-04-06 14:32:06 -0600

    Bases de animacion de seleccion

--- JavaScript/Scripts.js
+++ JavaScript/Scripts.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Variables
   let currentIndex = 0;
+  let cardClicked = false;
+
+  // Constantes
   const slides = document.querySelectorAll("#cardContainer > div");
   const totalSlides = slides.length;
+  const cards = document.querySelectorAll(".cards");
 
   function showSlide(index) {
     slides.forEach((slide, i) => {
@@ -25,5 +30,30 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("prevBtn").addEventListener("click", function () {
     currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
     showSlide(currentIndex);
+  });
+
+  cards.forEach((card) => {
+    card.addEventListener(`click`, function () {
+      if (!cardClicked) {
+        this.classList.add("card-grow");
+        this.classList.add("clicked");
+        cardClicked = true;
+
+        cards.forEach((otherCard) => {
+          if (otherCard !== this) {
+            otherCard.style.pointerEvents = "none";
+            otherCard.classList.add("fade-out");
+          }
+        });
+
+        document.getElementById("nextBtn").classList.add("fade-out");
+        document.getElementById("prevBtn").classList.add("fade-out");
+        document.getElementById("Title1").classList.add("fade-out");
+        document.getElementById("Title2").classList.add("fade-out");
+        document.getElementById("Title3").classList.add("fade-out");
+        document.getElementById("nextBtn").disabled = true;
+        document.getElementById("prevBtn").disabled = true;
+      }
+    });
   });
 });
