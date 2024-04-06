c73db1d5ef68a44c1790e8e9c19e1f5b17f66791
EfrenDaMo <david.mora.work@gmail.com>
2024-04-05 19:44:38 -0600

    animacion de cambio de carousel

--- JavaScript/Scripts.js
+++ JavaScript/Scripts.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     let currentIndex = 0;
     const slides = document.querySelectorAll('#cardContainer > div');
     const totalSlides = slides.length;
@@ -23,4 +23,31 @@ document.addEventListener('DOMContentLoaded', function() {
         currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
         showSlide(currentIndex);
     });
+}); */
+
+document.addEventListener("DOMContentLoaded", function () {
+  let currentIndex = 0;
+  const slides = document.querySelectorAll("#cardContainer > div");
+  const totalSlides = slides.length;
+
+  function showSlide(index) {
+    // Hide all slides by setting opacity to 0
+    slides.forEach((slide) => {
+      slide.classList.remove("show");
+    });
+    // Show the current slide by setting opacity to 1
+    slides[index].classList.add("show");
+  }
+
+  showSlide(currentIndex);
+
+  document.getElementById("nextBtn").addEventListener("click", function () {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    showSlide(currentIndex);
+  });
+
+  document.getElementById("prevBtn").addEventListener("click", function () {
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    showSlide(currentIndex);
+  });
 });
