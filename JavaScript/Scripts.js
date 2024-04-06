8b0c3b3734b37b73894c5ec547bb2490535073e2
EfrenDaMo <david.mora.work@gmail.com>
2024-04-05 20:13:54 -0600

    Arregle errores de vistas

--- JavaScript/Scripts.js
+++ JavaScript/Scripts.js
@@ -1,42 +1,18 @@
-/*document.addEventListener('DOMContentLoaded', function() {
-    let currentIndex = 0;
-    const slides = document.querySelectorAll('#cardContainer > div');
-    const totalSlides = slides.length;
-
-    function showSlide(index) {
-        //Esconde todas las tarjetas
-        slides.forEach(slide => {
-            slide.style.display = 'none';
-        });
-        //Muestra las tarjetas
-        slides[index].style.display = 'block';
-    }
-
-    showSlide(currentIndex);
-
-    document.getElementById('nextBtn').addEventListener('click', function() {
-        currentIndex = (currentIndex + 1) % totalSlides;
-        showSlide(currentIndex);
-    });
-
-    document.getElementById('prevBtn').addEventListener('click', function() {
-        currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-        showSlide(currentIndex);
-    });
-}); */
-
 document.addEventListener("DOMContentLoaded", function () {
   let currentIndex = 0;
   const slides = document.querySelectorAll("#cardContainer > div");
   const totalSlides = slides.length;
 
   function showSlide(index) {
-    // Hide all slides by setting opacity to 0
-    slides.forEach((slide) => {
-      slide.classList.remove("show");
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.classList.add("show");
+            slide.classList.remove("hide");
+        } else {
+            slide.classList.remove("show");
+            slide.classList.add("hide");
+        }
     });
-    // Show the current slide by setting opacity to 1
-    slides[index].classList.add("show");
   }
 
   showSlide(currentIndex);
