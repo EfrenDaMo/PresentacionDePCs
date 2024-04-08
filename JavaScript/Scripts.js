6301418845145b1c33b7b96bfdb9d11ce103ba71
EfrenDaMo <david.mora.work@gmail.com>
2024-04-07 21:43:54 -0600

    Funcion agregada a boton de regreso

--- JavaScript/Scripts.js
+++ JavaScript/Scripts.js
@@ -22,6 +22,57 @@ document.addEventListener("DOMContentLoaded", function () {
 
   showSlide(currentIndex);
 
+  document.getElementById("backBtn").addEventListener("click", function () {
+    const selectedCard = document.querySelector(".card-grow");
+    if (selectedCard) {
+      selectedCard.classList.remove(
+        "card-grow",
+        "clicked",
+        "centered-card",
+        "in-place"
+      );
+
+      setTimeout(() => {
+        cardClicked = false;
+
+        cards.forEach((otherCard) => {
+          otherCard.style.pointerEvents = "";
+          otherCard.classList.remove("fade-out");
+          otherCard.classList.add("fade-in");
+          setTimeout(() => {
+            otherCard.classList.remove("fade-in");
+          }, 60);
+        });
+
+        document.getElementById("nextBtn").classList.remove("fade-out");
+        document.getElementById("prevBtn").classList.remove("fade-out");
+        document.getElementById("Title1").classList.remove("fade-out");
+        document.getElementById("Title2").classList.remove("fade-out");
+        document.getElementById("Title3").classList.remove("fade-out");
+
+        // Now, add the fade-in class and enable the elements if needed
+        const elementsToFadeIn = [
+          document.getElementById("nextBtn"),
+          document.getElementById("prevBtn"),
+          document.getElementById("Title1"),
+          document.getElementById("Title2"),
+          document.getElementById("Title3"),
+        ];
+
+        elementsToFadeIn.forEach((element) => {
+          element.classList.add("fade-in");
+          element.disabled = false;
+          setTimeout(() => {
+            element.classList.remove("fade-in");
+          }, 60);
+        });
+      }, 50);
+
+      document.getElementById("backBtn").classList.remove("fade-in");
+      document.getElementById("backBtn").classList.add("fade-out");
+    }
+  });
+
   document.getElementById("nextBtn").addEventListener("click", function () {
     currentIndex = (currentIndex + 1) % totalSlides;
     showSlide(currentIndex);
@@ -43,7 +94,7 @@ document.addEventListener("DOMContentLoaded", function () {
           this.classList.add("centered-card");
           this.classList.add("in-place");
         }, 300);
-        
+
         cards.forEach((otherCard) => {
           if (otherCard !== this) {
             otherCard.style.pointerEvents = "none";
